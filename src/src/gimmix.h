4d5a499331250c79bde950b14fca2198c838faac
Priyank Gosalia <priyankmg@gmail.com>
2006-12-10 05:40:29 +0000

    === Gimmix version 0.3 Released === Lots of new features and a mountain of bug fixes. Refer changelog.

--- src/src/gimmix.h
+++ src/src/gimmix.h
@@ -18,7 +18,7 @@
 #include "gimmix-interface.h"
 
 #define APPNAME 		"Gimmix"
-#define VERSION 		"0.3-RC2"
+#define VERSION 		"0.3"
 #define APPURL			"http://gimmix.berlios.de/"
 
 /* main connect function */
