20b8e7a56ab1f9df35e519ed756ab1d759bbcfc6
Priyank Gosalia <priyankmg@gmail.com>
2008-09-14 07:43:05 +0000

    * Makefile.am: added gimmix-covers.c * gimmix-covers.c: implemented a cover download function using curl

--- src/src/gimmix-covers.c
+++ src/src/gimmix-covers.c
@@ -31,12 +31,67 @@
 #include <curl/easy.h>
 #include <libxml/xmlreader.h>
 
-#define AMAZON_KEY	14TBPBEBTPCVM7BY0C02
+#define COVERS_DIR	".gimmix/covers"
+#define COVERS_DBF	".gimmix/covers/covers.db"
+#define RESULT_XML	"cvr.xml"
+#define AMAZON_KEY	"14TBPBEBTPCVM7BY0C02"
 #define AMAZON_URL	"http://ecs.amazonaws.com/onca/xml?Service=AWSECommerceService&Operation=ItemSearch&SearchIndex=Music&ResponseGroup=Images,EditorialReview&AWSAccessKeyId=%s&%s=%s&%s=%s"
+
+static gboolean gimmix_covers_plugin_download (const char *url, const char *file);
 
 static void
 gimmix_covers_plugin_init (void)
 {
+	gchar	*cpath = NULL;
 
+	/* check if .gimmix/lyrics exists */
+	cpath = cfg_get_path_to_config_file (COVERS_DIR);
+	g_mkdir_with_parents (cpath, 00755);
+	
+	return;
 }
+
+static size_t
+__curl_write_func (void *ptr, size_t size, size_t nmemb, FILE *stream)
+{
+	return fwrite (ptr, size, nmemb, stream);
+}
+
+static size_t
+__curl_read_func (void *ptr, size_t size, size_t nmemb, FILE *stream)
+{
+	return fread (ptr, size, nmemb, stream);
+}
+
+static gboolean
+gimmix_covers_plugin_download (const char *url, const char *file)
+{
+	CURL		*curl;
+	CURLcode	res;
+	gboolean	ret = FALSE;
+	
+	curl = curl_easy_init ();
+	if (curl)
+	{
+		FILE *outfile = NULL;
+
+		path = g_strdup_printf ("%s/%s", cfg_get_path_to_config_file(COVERS_DIR), file);
+		outfile = fopen (path, "w");
+		curl_easy_setopt (curl, CURLOPT_URL, url);
+		curl_easy_setopt (curl, CURLOPT_WRITEDATA, outfile);
+		curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, __curl_write_func);
+		curl_easy_setopt (curl, CURLOPT_READFUNCTION, __curl_read_func);
+		res = curl_easy_perform (curl);
+		if (!res != 0)
+		{
+			ret = TRUE;
+		}
+		fclose (outfile);
+		curl_easy_cleanup (curl);
+		g_free (path);
+	}
+	
+	return ret;
+}
+
 
