9695cbfd3dd133bbb434ef233350947f2309dd89
Christoph Mende <mende.christoph@gmail.com>
2013-02-01 15:02:33 +0100

    Exit autogen on failure

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -x
 
-glib-gettextize --copy --force
-intltoolize --automake --copy --force
-aclocal
-autoconf
-autoheader
-automake --force-missing --add-missing --copy --foreign
+glib-gettextize --copy --force || exit 1
+intltoolize --automake --copy --force || exit 1
+aclocal || exit 1
+autoconf || exit 1
+autoheader || exit 1
+automake --force-missing --add-missing --copy --foreign || exit 1
